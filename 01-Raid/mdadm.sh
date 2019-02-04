e3e4b964c50786e5df3209966a6fad6055389d79
Nikki <nikki@elvees.com>
2019-02-04 15:42:45 +0300

    fs

--- 01-Raid/mdadm.sh
+++ 01-Raid/mdadm.sh
@@ -15,3 +15,18 @@ sudo mkdir /etc/mdadm
 #создать файл конфигурации
 sudo echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
 sudo mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >>/etc/mdadm/mdadm.conf
+#
+#создать GPT раздел
+sudo parted -s /dev/md0 mklabel gpt
+#создать партиции
+sudo parted /dev/md0 mkpart primary ext4 0% 20%
+sudo parted /dev/md0 mkpart primary ext4 20% 40%
+sudo parted /dev/md0 mkpart primary ext4 40% 60%
+sudo parted /dev/md0 mkpart primary ext4 60% 80%
+sudo parted /dev/md0 mkpart primary ext4 80% 100%
+#создать файловую систему на партициях
+for i in $(seq 1 5); do sudo mkfs.ext4 /dev/md0p$i; done
+#создать точки монтирования
+sudo mkdir -p /raid/part{1,2,3,4,5}
+#примонтировать
+for i in $(seq 1 5); do sudo mount /dev/md0p$i /raid/part$i; done
